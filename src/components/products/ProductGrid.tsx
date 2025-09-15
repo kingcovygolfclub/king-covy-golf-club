e9ecb8f531ad0791de04db9ec0d9259271344591
piercenoah <pierce.noah@gmail.com>
2025-09-15 02:34:10 -0400

    Fix remaining OptimizedImage reference in ProductGrid list view
    - Replace OptimizedImage with Image in ProductListItem component
    - This was causing the TypeScript build error
    - All image components now use standard Next.js Image
    - Build now passes successfully
    
    The deployment failed because there was still one OptimizedImage
    reference in the list view that I missed in the previous fix.

--- src/components/products/ProductGrid.tsx
+++ src/components/products/ProductGrid.tsx
@@ -111,7 +111,7 @@ const ProductGrid: React.FC<ProductGridProps> = ({ products, viewMode }) => {
     <div className="card group">
       <div className="flex space-x-4 p-4">
         <div className="relative w-24 h-24 flex-shrink-0">
-          <OptimizedImage
+          <Image
             src={product.images[0] || 'https://king-covy-assets.s3.amazonaws.com/products/placeholder-golf-club.svg'}
             alt={product.name}
             fill
